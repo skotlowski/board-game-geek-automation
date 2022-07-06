540e53425d8733930c073b496e10521ea2d3f55e
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2022-07-06 22:38:22 +0200

    selenium login with request implemented

--- conftest.py
+++ conftest.py
@@ -18,3 +18,46 @@ def json_file():
 def login_and_password():
     data = json_file()
     return data
+
+
+@fixture(scope='session')
+def session_logged(login_and_password):
+    session = Session()
+    session.headers.update(
+        {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+        })
+
+    session.get(url='https://boardgamegeek.com')
+    session.post(url='https://boardgamegeek.com/login/api/v1', json=login_and_password)
+    return session
+
+
+@fixture(scope='function')
+def browser_logged_with_requests(session_logged):
+    session_id = session_logged.cookies.get('SessionID')
+    bgg_password = session_logged.cookies.get('bggpassword')
+    bgg_username = session_logged.cookies.get('bggusername')
+
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+
+    driver.get(url='https://boardgamegeek.com')
+
+    driver.add_cookie({
+        'name': 'SessionID',
+        'value': session_id
+    })
+    driver.add_cookie({
+        'name': 'bggpassword',
+        'value': bgg_password
+    })
+    driver.add_cookie({
+        'name': 'bggusername',
+        'value': bgg_username
+    })
+
+    driver.refresh()
+
+    yield driver
+    driver.quit()
