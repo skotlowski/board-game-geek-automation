70d63af67666550fcc4e9ab6d58f719eea212701
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2022-07-12 23:54:29 +0200

    login_with_selenium_test_implementation

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+from time import sleep as wait
 from requests import Session
 from pytest import mark
 from selenium.webdriver.common.by import By
@@ -24,3 +25,12 @@ class LoginTests:
     def test_login_with_selenium(self, login_and_password, browser_session):
         login_data = login_and_password
         driver = browser_session
+        driver.find_element(By.CSS_SELECTOR, 'a[ggloginbutton=""]').click()
+        driver.find_element(By.CSS_SELECTOR, 'input[name="username"]').send_keys(login_data['credentials']['username'])
+        driver.find_element(By.CSS_SELECTOR, 'input[name="password"]').send_keys(login_data['credentials']['password'])
+        wait(1)
+        driver.find_element(By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary"]').click()
+        wait(1)
+        page_source = driver.page_source
+
+        assert 'Sign Out' in page_source
