d2ab761c885d665f1a0c241167cbf0485732e7b3
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2022-07-21 23:32:59 +0200

    fake username and password method implementation

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 from pytest import fixture
 from requests import Session
 from selenium import webdriver
+from faker import Faker
 import json
 
 
@@ -9,6 +10,14 @@ def json_file():
     with open(pass_file) as json_file:
         data = json.load(json_file)
         return data
+
+
+@fixture(scope='function')
+def fake_user():
+    faker = Faker()
+    user_name = faker.name().replace(' ', '')
+    password = faker.password(length=10)
+    return [user_name, password]
 
 
 @fixture(scope='session')
