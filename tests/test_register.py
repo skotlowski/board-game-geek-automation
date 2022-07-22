b4619de451e4360cd9bee59101512388d2ca2ab5
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2022-07-22 08:50:53 +0200

    registration user with requests implementation

--- tests/test_register.py
+++ tests/test_register.py
@@ -5,9 +5,14 @@ from pytest import mark
 class RegisterTests:
 
     @mark.request
-    def test_registration_user(self):
-        pass
+    def test_registration_user(self, session_logged, fake_user):
+        session = session_logged
+        response = session.post(url='https://boardgamegeek.com/api/accounts', json=fake_user)
+        expected_response = 200
+
+        assert response.status_code == expected_response
 
     @mark.selenium
     def test_registration_with_selenium(self):
         pass
+
